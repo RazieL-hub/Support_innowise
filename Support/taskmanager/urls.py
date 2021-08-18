ad6f2ab0b2d4087ba063379ddf2d9236e97079cb
RazieL <saalearn@gmail.com>
2021-08-18 19:37:13 +0300

    Correct ticket detail and all list

--- Support/taskmanager/urls.py
+++ Support/taskmanager/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from taskmanager.views import TicketCreateView
+from taskmanager.views import TicketCreateView, TicketListView, TicketDetailView
 
 urlpatterns = [
     path('create_ticket/', TicketCreateView.as_view(), name='create_ticket'),
+    path('ticket/', TicketListView.as_view(), name='all_tickets'),
+    path('ticket/<int:pk>/', TicketDetailView.as_view(), name='ticket_by_id'),
 ]
--- Support/taskmanager/views.py
+++ Support/taskmanager/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from taskmanager.models import Ticket
-from taskmanager.serializers import TicketCreateSerializer
-from rest_framework.views import APIView, Response
+from taskmanager.serializers import TicketCreateSerializer, TicketListSerializer, TicketDetailSerializer
+from rest_framework.views import APIView
 from rest_framework import generics
+from rest_framework.response import Response
 
 
 class TicketCreateView(generics.CreateAPIView):
     serializer_class = TicketCreateSerializer
+
+
+class TicketListView(APIView):
+    """Вывод полного списка тикетов"""
+
+    def get(self, request):
+        tickets = Ticket.objects.all()
+        serializer = TicketListSerializer(tickets, many=True)
+        return Response(serializer.data)
+
+
+class TicketDetailView(APIView):
+    """Вывод конкретного тикетов"""
+
+    def get(self, request, pk):
+        tickets = Ticket.objects.get(id=pk)
+        serializer = TicketDetailSerializer(tickets)
+        return Response(serializer.data)
