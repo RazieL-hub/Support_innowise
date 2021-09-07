a2437e08628509bcbe23c3d7ba5b6511891b5c6e
RazieL <saalearn@gmail.com>
2021-09-07 19:36:17 +0300

    kill static

--- Support/config/urls.py
+++ Support/config/urls.py
@@ -20,7 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('api-auth/', include('rest_framework.urls')),
-    path('ckeditor/', include('ckeditor_uploader.urls')),
 
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
